f831ba8bbfa713aa5e502cca70694a92e69a0e19
Kaliane <kaliane.pemi@gmail.com>
2021-10-19 21:12:17 -0300

    add logic

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculo.setOnClickListener {
+            calcular()
+        }
+    }
+    fun calcular(){
+        val nText = binding.digitaNumeros1.text.toString()
+        val numero1 = nText.toDouble()
+        val nText2 = binding.digitaNumeros2.text.toString()
+        val numero2 = nText2.toDouble()
+        val selecao = binding.operacoes.checkedRadioButtonId
+        val resultado = when (selecao){
+            R.id.divisao -> numero1 / numero2
+            R.id.multiplicacao -> numero1 * numero2
+            R.id.soma -> numero1 + numero2
+            else -> numero1 - numero2
+        }
+        binding.resultado.text = resultado.toString()
     }
 }
